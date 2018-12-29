8e267b0877e43af43979a3c3f4afcf04a1085195
Clarence Pollard <pollacm@gmail.com>
2018-12-28 23:05:34 -0500

    mobile working for navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxil';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerClosedClicked = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => 
+        {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
+
+    render() {        
+        return <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedClicked}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>;
+    }
+}
+    
+export default Layout;
